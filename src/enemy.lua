7b6217b3f222f01aa105b173cd499788156b60b4
Jeremy Gustine <jeremy@crafted.solutions>
2020-10-10 14:22:10 -0600

    Enemies move towards player

--- src/enemy.lua
+++ src/enemy.lua
@@ -1,36 +1,39 @@
 function make_enemy(_x,_y, _column)
   return {
-    start_x=_x,
-    x=_x,
-    y=_y,
-    column=_column,
-    sprite=1,
-    dx=1,
-    range=40,
-    last_update=0,
-    update_rate=10,
-    update=function(self)
-      self.last_update+=1
-      if self.last_update==self.update_rate then
-        self.last_update=0  
+    start_x = _x,
+    x = _x + 1, -- start 1 pixel to the right so we don't immediately move down
+    y = _y,
+    column = _column,
+    sprite = 1,
+    dx = 1,
+    dy = 3,
+    range = 40,
+    last_update = 0,
+    update_rate = 10,
+    update = function(self)
+      self.last_update += 1
+      if self.last_update == self.update_rate then
+        self.last_update = 0
         
-        if self.x==(self.start_x+self.range) then
-          self.dx=-1
+        if self.x >= (self.start_x + self.range) then
+          self.dx =- 1
+          self.y += self.dy
         end
-        if self.x==self.start_x then
-          self.dx=1
+        if self.x == self.start_x then
+          self.dx = 1
+          self.y += self.dy
         end
-        self.x+=self.dx
+        self.x += self.dx
 
-        if self.sprite==1 then 
-          self.sprite=2 
-        elseif self.sprite==2 then 
-          self.sprite=1 
+        if self.sprite == 1 then
+          self.sprite = 2
+        elseif self.sprite == 2 then
+          self.sprite = 1
         end
       end
     end,
-    draw=function(self)
-      spr(self.sprite,self.x,self.y)
+    draw = function(self)
+      spr(self.sprite, self.x, self.y)
     end
   }
 end
